36cb069ba98281679f9cbfdc45c67a06099fcad2
Fabricio <fabricio.riff2@gmail.com>
2020-08-18 10:41:01 -0300

    estrutura de projeto

---
+++ estrutura_projeto/lib/main.dart
@@ -0,0 +1,7 @@
+void main(List<String> arguments) {
+  if(arguments[0] == 'hackear'){
+    print('hackeamento concluido');
+  } else if(arguments[0] == 'imprimir'){
+    print('executando impressão');
+  }
+}
